1ed3cbe17c0bded64da301ead0b69147b33c8424
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-10-23 13:20:37 +0900

    feat: 모든 엔티티가 상속받을 기본 엔티티 구현

---
+++ src/main/java/grit/stockIt/global/common/BaseEntity.java
@@ -0,0 +1,39 @@
+package grit.stockIt.global.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+// 모든 엔티티가 공통으로 상속받을 기본 엔티티
+// 생성일시, 수정일시를 자동으로 관리하고, 삭제일시를 통해 소프트 삭제를 구현함.
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity { // 추상클래스로 함
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime deletedAt;
+
+    public void softDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
