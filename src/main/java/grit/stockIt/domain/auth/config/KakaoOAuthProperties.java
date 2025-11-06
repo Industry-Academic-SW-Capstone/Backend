f45dbf4a52bc0bc18d2d7e3ac3f53149abccffeb
최재현 <chlwogus180@naver.com>
2025-11-06 15:30:16 +0900

    공식 ip 발급 완료 + bean 문제 해결

--- src/main/java/grit/stockIt/domain/auth/config/KakaoOAuthProperties.java
+++ src/main/java/grit/stockIt/domain/auth/config/KakaoOAuthProperties.java
@@ -3,10 +3,13 @@ package grit.stockIt.domain.auth.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
 
 @Getter
 @Setter
+
 @ConfigurationProperties(prefix = "kakao")
+@Primary
 public class KakaoOAuthProperties {
     private String restApiKey;
     private String redirectUri;
