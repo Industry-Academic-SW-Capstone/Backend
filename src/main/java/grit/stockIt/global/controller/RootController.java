f5fa414fabcf3d5f9a188b72c156ca1dea7f912f
최재현 <chlwogus180@naver.com>
2025-11-08 08:16:10 +0900

    http://www.stockit.live 접속가능

---
+++ src/main/java/grit/stockIt/global/controller/RootController.java
@@ -0,0 +1,14 @@
+package grit.stockIt.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "StockIt backend is up.";
+    }
+}
+
