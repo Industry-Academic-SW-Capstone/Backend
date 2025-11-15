3cb38dd32fbaf5067f7f6a55aa2d7d2a5bd30a3d
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-11-15 20:52:31 +0900

    chore: FCM 초기화 설정

--- src/main/java/grit/stockIt/StockItApplication.java
+++ src/main/java/grit/stockIt/StockItApplication.java
@@ -1,13 +1,14 @@
 package grit.stockIt;
 
 import grit.stockIt.domain.auth.config.KakaoOAuthProperties;
+import grit.stockIt.global.config.FirebaseProperties;
 import grit.stockIt.global.config.KisApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableConfigurationProperties({KisApiProperties.class, KakaoOAuthProperties.class})
+@EnableConfigurationProperties({KisApiProperties.class, KakaoOAuthProperties.class, FirebaseProperties.class})
 @EnableScheduling
 @SpringBootApplication
 public class StockItApplication {
