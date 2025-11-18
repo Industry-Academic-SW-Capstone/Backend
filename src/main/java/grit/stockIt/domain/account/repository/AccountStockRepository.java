86214a11edff1becfeed46b04443b0a30d1dc6cf
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-11-19 02:44:32 +0900

    faet: 패치조인으로 n+1문제 방지하여 보유종목 조회 구현

--- src/main/java/grit/stockIt/domain/account/repository/AccountStockRepository.java
+++ src/main/java/grit/stockIt/domain/account/repository/AccountStockRepository.java
@@ -4,10 +4,21 @@ import grit.stockIt.domain.account.entity.Account;
 import grit.stockIt.domain.account.entity.AccountStock;
 import grit.stockIt.domain.stock.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountStockRepository extends JpaRepository<AccountStock, Long> {
 
     Optional<AccountStock> findByAccountAndStock(Account account, Stock stock);
+
+    // 계좌의 보유종목 목록 조회 (Stock JOIN FETCH로 N+1 방지)
+    @Query("SELECT as FROM AccountStock as " +
+           "JOIN FETCH as.stock s " +
+           "WHERE as.account.accountId = :accountId " +
+           "AND as.quantity > 0 " +
+           "ORDER BY s.name")
+    List<AccountStock> findByAccountIdWithStock(@Param("accountId") Long accountId);
 }
